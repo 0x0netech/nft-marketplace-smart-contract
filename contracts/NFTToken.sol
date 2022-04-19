e164eac5e296043a466c5537babf3914e65f0053
druej-innotech <stephens.druej@gmail.com>
2022-04-19 11:00:07 +0530

    Nappy token contract added 🚀

--- contracts/NFTToken.sol
+++ contracts/NFTToken.sol
@@ -2,8 +2,29 @@
 
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract NFTToken {
+contract NFTToken is ERC721URIStorage {
     
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address NFTMarket) ERC721('NappyNFT' ,'NPFT') {
+        contractAddress = NFTMarket;
+    }
+
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        _setApprovalForAll(_msgSender(),contractAddress, true);
+        return newTokenId;
+    }
 }
