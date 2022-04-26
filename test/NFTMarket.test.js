cb40b821b25f358412e49646fddb872894ee9a65
druej-innotech <stephens.druej@gmail.com>
2022-04-26 18:34:43 +0530

    buyNft test case completed 🚀

--- test/NFTMarket.test.js
+++ test/NFTMarket.test.js
@@ -21,20 +21,29 @@ describe('NFTMarket', () => {
     const NFTMarket = await ethers.getContractFactory('NFTMarket');
     const nftMarket = await NFTMarket.deploy(nappyTokenAddress);
     await nftMarket.deployed();
-    const nftAddress = nftMarket.address;
+    const nftContractAddress = nftMarket.address;
 
     const NFTToken = await ethers.getContractFactory('NFTToken');
-    const nftToken = await NFTToken.deploy(nftAddress);
+    const nftToken = await NFTToken.deploy(nftContractAddress);
     await nftToken.deployed();
-    const nftContractAddress = nftToken.address;
+    const nftAddress = nftToken.address;
 
     await nftToken.createToken('https://www.json.com/JuG8');
 
-    listNFT = await nftMarket.listNft(nftContractAddress, 1, 234, 11);
+    listNFT = await nftMarket.listNft(nftAddress, 1, 234, 11);
     await nftToken.balanceOf(owner.address);
 
-    await nappyToken.approve(addr1.address, 200000);
-    await nftToken.balanceOf(addr1.address);
+    await nappyToken.approve(
+      nftContractAddress,
+      BigNumber.from(23400).mul(BigNumber.from(10).pow(18))
+    );
+    await nftToken.balanceOf(owner.address);
+
+    await nftMarket.connect(owner).buyNft(1);
+
+    // await nftMarket.connect(addr1.address).buyNft(1);
+    // await buyNFT.wait();
+    // await nftToken.balanceOf(nftAddress);
 
     const amount = await nftToken.balanceOf(nftAddress);
     console.log(amount.toString());
